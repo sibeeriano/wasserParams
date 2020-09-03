e0cbf64dd0bda7b26a04493d8e0a6f1335d92bc0
facu vara <sibeeriano@gmail.com>
2020-09-03 16:25:03 +0200

    ahhhh x2

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var productosRouter = require('./routes/producto');
 
 var app = express();
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/productos', productosRouter);
 
 // catch 404 and forward to error handler
--- controllers/productoControllers.js
+++ controllers/productoControllers.js
@@ -2,54 +2,66 @@ const fs = require('fs');
 const path = require('path')
 let dbProducto = require('../data/database')
 
+
 module.exports={
     listar:function(req, res){
-        res.render('PRODUCTOS',{
+        res.render('productos',{
             title:"Nuestros productos",
             dbProducto:dbProducto
         });
     },
+
+    producto: function(req, res) { //detalle de producto
+        let id=req.params.id
+
+        let producto = dbProduct.forEach((producto) => {
+            return id == producto.id});
+        
+            res.render('producto',{
+                producto:producto
+        });
+    },
     
-    cargaProducto:function(req, res) {
-        res.render('carga', { title: 'Admin carga' });
-    },
-    detalleProducto: function(req, res) {
-        res.render('DETALLEPRODUCTO', {
-          title: "Detalle de producto" //falta logica
-        });
-    },
-    carrito:function(req, res) {
-        res.render('Carrito', {
-          title: "Carrito de compras" //falta logica
-        });
-    },
-    cateProducto:function(req,res){
-        let catProducto = req.params.catProducto
-        res.render('categoria',{
-            title:"wasser",
-            catProducto:catProducto,
-            dbProducto:dbProducto 
+    categorias:function(req,res){
+
+        let cat=req.params.category
+
+        let categoria = dbProducto.forEach(producto => {
+            return cat == producto.category
+         })
+         res.render('categorias',{
+            title: "WASSER",
+            categoria: categoria,
         })
     }
-    /*publicarProducto: function(req,res,next){
-        let lastID = 1;
-
-        dbProducto.forEach(producto=>{
-            if(producto.id > lastID){
-                lastID = producto.id
-            }
-        })
-        let nuevoProducto={
-            id: lastID + 1,
-            nombre: req.body.nombre.trim(),
-            precio: Number(req.body.precio),
-            descuento: Number(req.body.descuento),
-            categoriaProducto: req.body.catergoria,
-            descripcion:req.body.descripcion,
-            imagen: (req.files[0])?req.files[0].filename:"productoMuestra.png"
-        };
-        dbProducto.push(nuevoProducto);
-        fs.writeFileSync(path.join(__dirname,"..",'data',"productosDataBase.json"),JSON.stringify(dbProducto),'utf-8');
-        res.redirect('/productos');
-    }*/
+    
 }
+
+
+
+
+         /*
+         let cocina = dbProducto.filter(producto => {
+           return producto.category == "cocina"
+         })
+         let bachas = dbProducto.filter(producto => {
+         return producto.category== "bachas"
+         })
+         let sanitarios = dbProducto.filter(producto => {
+         return producto.category== "sanitarios"
+         })
+        let bañeras = dbProducto.filter(producto => {
+         return producto.category== "bañeras"
+         })
+        let accesorios = dbProducto.filter(producto => {
+         return producto.category== "accesorios"
+         })*/
+
+         
+           /*
+           cocina: cocina,
+           bachas: bachas,
+           sanitarios: sanitarios,
+           bañeras: bañeras,
+           accesorios: accesorios
+*/
